8544f6f06070414452803f0548ca5e77f21cd921
zafergunayonalan <zafergunay7@gmail.com>
2024-11-13 17:50:09 +0300

    project-adjustments

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@material-tailwind/react"
+
+const App = () => {
+  return (
+    <div>
+      <Button>Buuton</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { ThemeProvider } from "@material-tailwind/react";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
