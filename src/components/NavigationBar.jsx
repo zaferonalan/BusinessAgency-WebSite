29075a6406ad39eaa0e1b283c6e118c9a17158d2
zafergunayonalan <zafergunay7@gmail.com>
2024-11-15 20:42:04 +0300

    navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,32 @@
+import { Navbar, Typography } from "@material-tailwind/react"
 
 const NavigationBar = () => {
   return (
-    <div>NavigationBar</div>
+    <Navbar fullWidth className="bg-[#030712] px-3">
+      <div className="px-16 border-2 mx-auto">
+        <div className="flex justify-between items-center px-5">
+          {/* Logo */}
+          <div>
+            <Typography variant="h2" className="text-[#33CCCC]">Logo</Typography>
+          </div>
+          {/* nav */}
+          <nav>
+            
+          </nav>
+        </div>
+      </div>
+
+    </Navbar>
+    // <div className="bg-[#030712] py-3 px-5">
+    //   <div className="mx-auto">
+    //     <div className="flex justify-between items-center px-5">
+    //       {/* Logo */}
+    //       <div>
+    //         <Typography variant="h3" className="text-[#33CCCC]">Logo</Typography>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
