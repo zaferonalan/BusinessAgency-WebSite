9c6fc4a01f1e8e03cfa080ed990fc63450ba79f7
zafergunayonalan <zafergunay7@gmail.com>
2024-11-27 17:52:56 +0300

    Footer-created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./components/About"
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import NavigationBar from "./components/NavigationBar"
 import Services from "./components/Services"
@@ -14,6 +15,7 @@ const App = () => {
       <Services/>
       <Testimonials/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#030712] p-6 text-center">
+        <p className="text-gray-300">&copy; 2024 <span className="text-[#33CCCC]">Webelite</span>. All rights reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
